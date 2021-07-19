3f5aa4e4050fd2a88b3a9eba868a937613e8cbbe
Enile <87643954+Enile@users.noreply.github.com>
2021-07-19 12:46:02 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, redirect
+from android.permissions import Permission, request_permissions
+request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
+
+app=Flask(__name__)
+
+app.route("/")
+def andriod():
+    return redirect('http://www.enile.co/')
+
+if __name__=="__main__":  
+    app.run(debug=False, port=8080)
